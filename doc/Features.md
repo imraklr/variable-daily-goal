a7bfcd8163845074cb2b007caeb9402b6f995d6e
imraklr <44721620+imraklr@users.noreply.github.com>
2026-06-06 20:12:51 +0530

    structural makeup 1

---
+++ doc/Features.md
@@ -0,0 +1,28 @@
+# Features
+
+## Version: 0.1.0
+
+### Functional Requirements (FRs) (The system shall [perform some action].)
+
+1. The system shall be able to replicate the entire day's schedule for the whole week in count.
+2. The system shall be able to CRUD items in schedule with stateful cascading.
+3. The system shall have a text input for daily comments, whose field shall remain open for the next half day.
+4. The system shall be able to show weeks' schedule at one place.
+5. The system shall display the basic information, preferrably on center top of screen, the date and weather in format:
+    HH:MM DD:MM:YYYY 33°C Thu
+6. The system shall have a timing feature which can be used to allocate time to tasks.
+7. The system shall be able to use vertical and horizontal time structure stateful cascading:
+    a. Vertical Stateful Cascading: All tasks in the day are assigned with a default of 30 minute gaps.
+    b. Horizontal Stateful Cascading: All tasks with various timings on a day are statefully cascaded to the other days in that week.
+8. The system shall preserve week work format until altered.
+9. The system shall be able to show the weekly progress.
+
+### Non-Functional Requirements (FRs) (The system shall achieve/maintain/support [quality metric].)
+
+1. The system shall maintain a 24x7 uptime.
+
+## Version: Planned
+
+### Planned Functional Requirements (FRs) [The system shall "perform some action."]
+
+1. The system shall show the area graph and trendline of progress with choice of trendline suggested for various tasks.
--- src/main.rs
+++ src/main.rs
@@ -1,1 +1,94 @@
-fn main() {}
+use std::fs;
+
+use actix_web::{App, HttpResponse, HttpServer, Responder, get, web};
+use local_ip_address::local_ip;
+use mdns_sd::{ServiceDaemon, ServiceInfo};
+
+
+fn configure_mdns() {
+    // Get local LAN IP
+    let ip = local_ip().unwrap();
+    // mDNS daemon
+    let mdns = ServiceDaemon::new().expect("Failed to create mDNS daemon");
+    // Service type
+    let service_type = "_http._tcp.local.";
+    // Instance name
+    let instance_name = "me-daily";
+    // Hostname
+    let host_name = "me-daily.local.";
+
+    // Advertise service
+    let service_info = ServiceInfo::new(
+        service_type,
+        instance_name,
+        host_name,
+        ip.to_string(),
+        8000,
+        None,
+    )
+    .unwrap();
+    let service_info = service_info.enable_addr_auto();
+
+    mdns.register(service_info)
+        .expect("Failed to register mDNS service");
+
+    println!("Open on phone:");
+    println!("http://me-daily.local:8000");
+}
+
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    configure_mdns();
+
+    HttpServer::new(|| {
+        App::new()
+            .service(static_handler)
+            .route("/", web::get().to(landing))
+    })
+    .workers(3)
+    .bind(("0.0.0.0", 8000))?
+    .run()
+    .await
+}
+
+macro_rules! static_path_prepend {
+    ($x:expr) => {
+        concat!(
+            env!("CARGO_MANIFEST_DIR"),
+            concat!(
+                "/static/", $x
+            )
+        )
+    };
+}
+
+use std::path::PathBuf;
+#[get("/static/{path:.*}")]
+async fn static_handler(path: web::Path<String>) -> HttpResponse {
+    let file_path = PathBuf::from(static_path_prepend!("")).join(path.into_inner());
+
+    let bytes = std::fs::read(&file_path).unwrap();
+
+    let mime = mime_guess::from_path(&file_path)
+        .first_or_octet_stream();
+
+    HttpResponse::Ok()
+        .content_type(mime.as_ref())
+        .body(bytes)
+}
+
+async fn landing() -> impl Responder {
+    match fs::read_to_string(static_path_prepend!("landing/landing.html")) {
+        Ok(html) => HttpResponse::Ok()
+            .content_type("text/html; charset=utf-8")
+            .body(html),
+
+        Err(err) => {
+            eprintln!("Failed to read static: {err}");
+
+            HttpResponse::InternalServerError()
+                .body("Failed to load page")
+        }
+    }
+}
